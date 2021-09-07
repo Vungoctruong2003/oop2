3dffa11858b0b282a055931bac2446deb8448a36
Ngọc Trưởng <truongvugh2003@gmail.com>
2021-09-07 09:10:21 +0700

    [Bài tập] Game đơn giản - di chuyển đối tượng

--- simple_game.js
+++ simple_game.js
@@ -1,52 +1,66 @@
-function Hero(image, top, left, size){
-    this.image = image;
-    this.top = top;
-    this.left = left;
-    this.size = size;
-    this.speed = 50;
-
-    this.getHeroElement = function(){
-        return '<img width="'+ this.size + '"' +
-            ' height="'+ this.size + '"' +
-            ' src="' + this.image +'"' +
-            ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+class Hero {
+    constructor(image, top, left, size) {
+        this.image = image;
+        this.top = top;
+        this.left = left;
+        this.size = size;
+        this.flagmove = 'right'
     }
 
-    this.moveRight = function(){
-        this.left += this.speed;
-        console.log('ok: ' + this.left);
+    getHeroElement() {
+        return '<img width="' + this.size + '"' +
+            ' height="' + this.size + '"' +
+            ' src="' + this.image + '"' +
+            ' style="top: ' + this.top + 'px; left:' + this.left + 'px;position:absolute;" />';
     }
-    this.moveDown = function(){
-        this.top += this.speed;
-        console.log('ok: ' + this.top);
+
+    moveLeft() {
+        this.left -= 20
     }
-    this.moveUp = function(){
-        this.top -= this.speed;
-        console.log('ok: ' + this.top);
+
+    moveRight() {
+        this.left += 20
     }
-    this.moveLeft = function(){
-        this.left -= this.speed;
-        console.log('ok: ' + this.left);
+
+    moveTop() {
+        this.top -= 20
     }
+
+    moveDown() {
+        this.top += 20
+    }
+
+    moveHero() {
+        if (this.left >= window.innerWidth - this.size && this.top === 0) {
+            this.flagmove = 'down'
+        } else if (this.top >= window.innerHeight - this.size && this.left >= window.innerWidth - this.size) {
+            this.flagmove = 'left'
+        } else if (this.left === 0 && this.top >= window.innerHeight - this.size) {
+            this.flagmove = 'top'
+        } else if(this.left===0 && this.top===0) {
+            this.flagmove = 'right'
+        }
+        switch (this.flagmove) {
+            case "right":
+                this.moveRight()
+                break
+            case "down":
+                this.moveDown()
+                break
+            case "left":
+                this.moveLeft()
+                break
+            case "top":
+                this.moveTop()
+                break
+        }
+        document.getElementById('game').innerHTML = this.getHeroElement()
+    }
+
+
 }
 
-var hero = new Hero("https://cuongphim.com/wp-content/uploads/2018/09/Spectrum-Monica-Rambeau-Marvel-Comics-cuong-phim-1024x512.jpg", 20, 30, 200);
-
-function start(){
-    if(hero.left < window.innerWidth - hero.size){
-        hero.moveRight();
-    }
-    if(hero.left > window.innerWidth - hero.size){
-        hero.moveLeft();
-    }
-    if(hero.top < window.innerHeight - hero.size){
-        hero.moveDown();
-    }
-    if(hero.top > window.innerHeight - hero.size){
-        hero.moveUp();
-    }
-    document.getElementById('game').innerHTML = hero.getHeroElement();
-    setTimeout(start, 500)
-}
-
-start();
+let hero = new Hero('https://ae01.alicdn.com/kf/HTB1pmY1MpXXXXawXpXXq6xXFXXXK/Doraemon-Nobita-Nobi-Cosplay-Costume-Anime-Custom-Made-Uniform.jpg', 0, 0, 200);
+setInterval(function () {
+    hero.moveHero()
+}, 50)
